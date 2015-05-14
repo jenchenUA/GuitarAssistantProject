8fabcded0e2f4c677f5ea20006c5ff64168d3831
Evgenyy Zadoyannyy <djjekz@gmail.com>
2015-05-14 22:33:16 +0300

    Create MainActivity with NavigationDrawer and Fragment

---
+++ GuitarAssistantProject/app/src/main/java/jenchenua/guitarassistantproject/MainActivity.java
@@ -0,0 +1,74 @@
+package jenchenua.guitarassistantproject;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+
+import com.mikepenz.materialdrawer.Drawer;
+import com.mikepenz.materialdrawer.accountswitcher.AccountHeader;
+import com.mikepenz.materialdrawer.model.interfaces.IDrawerItem;
+
+import jenchenua.guitarassistantproject.utils.Utils;
+
+
+public class MainActivity extends AppCompatActivity {
+    private final static String LOG_TAG = "MainActivity";
+
+    private Drawer.Result drawerResult = null;
+    private AccountHeader.Result headerResult = null;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //Replace standard ActionBar for Toolbar
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        if (toolbar != null) {
+            setSupportActionBar(toolbar);
+        }
+
+        headerResult = Utils.getAccountHeader(MainActivity.this, savedInstanceState);
+        drawerResult = Utils.createDrawer(MainActivity.this, toolbar, headerResult);
+        drawerResult.setSelectionByIdentifier(1, false); // Set proper selection
+
+        drawerResult.openDrawer();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_scales, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (drawerResult != null && drawerResult.isDrawerOpen()) {
+            drawerResult.closeDrawer();
+        } else {
+            super.onBackPressed();
+        }
+    }
+}
