88e8f4f3c025f58fbeec13e84e11812a44f8326a
Evgenyy Zadoyannyy <djjekz@gmail.com>
2015-06-01 10:56:16 +0300

    Add FingeringDrawing class

---
+++ GuitarAssistantProject/app/src/main/java/jenchenua/guitarassistantproject/draw/FingeringDrawing.java
@@ -0,0 +1,17 @@
+package jenchenua.guitarassistantproject.draw;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.util.AttributeSet;
+import android.view.View;
+
+public class FingeringDrawing extends View {
+    public FingeringDrawing(Context context, AttributeSet attributeSet) {
+        super(context, attributeSet);
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+    }
+}
